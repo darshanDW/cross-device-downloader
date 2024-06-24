2bf7b7383ed486cbd9bafed4f2928ba7ac0b6ffc
unknown <darshanjadhav775@gmail.com>
2024-06-24 12:05:11 +0530

    final basic version

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
 
 
 chrome.downloads.onCreated.addListener((downloadItem) => {
-    chrome.storage.sync.get(['server_url'], (data) => {
+    chrome.storage.sync.get('server_url', (data) => {
         const server_url = data.server_url;
 
 
 
-        console.log(server_url);
-        console.log(downloadItem.url);
+
+        const endpoint = '/download';
+
         const send = async () => {
             try {
-                const response = await fetch('https://bdb9-106-66-28-34.ngrok-free.app/download', {
+                const response = await fetch(`${server_url.trim()}${endpoint}`, {
                     method: 'POST',
                     headers: { 'content-type': 'application/json' },
                     body: JSON.stringify({ url: downloadItem.url })
                 });
                 const data = await response.json();
-                console.log(data);
-
+                alert(data);
             } catch (err) {
-                console.log(err);
+                alert(err)
             }
         }
 
@@ -27,7 +27,7 @@ chrome.downloads.onCreated.addListener((downloadItem) => {
         if (server_url) {
             send();
         } else {
-            console.log("not get url");
+            alert("not get url");
         }
     });
 });
--- popup.js
+++ popup.js
@@ -1,12 +1,13 @@
 document.getElementById('setting-form').addEventListener('submit', function (event) {
     event.preventDefault();
     const server_url = document.getElementById('server_url').value;
-    chrome.storage.sync.set({ server_url, target_path }, function () {
-        console.log("done")
+    chrome.storage.sync.set({ server_url: server_url }, function () {
+        alert("done");
     })
 
+
+
     console.log(server_url);
-    console.log(target_path);
 
 
 
